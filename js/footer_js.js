66cef8eb04a15cee32db5861ec30d72252ff9023
Thurtwings <59878499+Thurtwings@users.noreply.github.com>
2023-04-04 14:26:58 +0200

    DONE: Hotfix login.php WIP: profile page TODO: profile page

--- js/footer_js.js
+++ js/footer_js.js
@@ -1,7 +1,8 @@
 // Charger le footer depuis le fichier footer.php
 fetch("../php/footer.php")
 .then(response => response.text())
-.then(data => {
+.then(data => 
+  {
   const footerContainer = document.querySelector("#footer-container");
   footerContainer.innerHTML = data;
 });
--- php/register.php
+++ php/register.php
@@ -3,15 +3,8 @@ require "../php/pdo_connection.php";
 session_start();
 
 
-/* if($_POST['password'] !== $_POST['passwordVerif'])
-{?>
-    <p>Veuillez vérifier votre mot de passe </p>
-    <?php
-    exit;
-} */
-
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+{
     $username = $_POST['username'];
     $password = $_POST['password'];
 
@@ -23,6 +16,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     {
         $error = "nom d'utilisateur déjà pris";
     } 
+    elseif($username === $password)
+    {
+        $error = "Le mot de passe et le nom d'utilisateur ne peuvent pas etre égaux";
+    }
     else 
     {
         $hash = password_hash($password, PASSWORD_DEFAULT);
@@ -40,40 +37,50 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet"/>
-    <title>Register</title>
-</head>
-<body>
-    <div id="navbar-container"></div>
-    <br>
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet"/>
+        <title>Register</title>
+    </head>
+    <body>
+        <div id="navbar-container"></div>
         <br>
         <br>
-    <h1 class="text-center">Créer un compte</h1>
-    <?php if (isset($error)) { ?>
-        
-        <p><?= $error ?></p>
-    <?php } ?>
-    <form action="../php/register.php" method="POST" class="text-center">
-        <label for="username">Pseudonyme:</label><br>
-        <input type="text" name="username" id="username"><br>
-        <label for="password">Mot de passe:</label><br>
-        <input type="password" name="password" id="password"><br>
-        <label for="password">Répeter mot de passe:</label><br>
-        <input type="password" name="passwordVerif" id="password"><br>
+        <br>
+        <h1 class="text-center">Créer un compte</h1>
+        <?php 
+        if (isset($error)) 
+        { ?>
+            <p class="text-center text-danger"><?= $error ?></p>
+        <?php 
+        } ?>
+        <form action="../php/register.php" method="POST" class="text-center">
+            <label for="username">Pseudonyme:</label>
+            <br>
+            <input type="text" name="username" id="username">
+            <br>
+            <label for="password">Mot de passe:</label>
+            <br>
+            <input type="password" name="password" id="password">
+            <br>
+            <label for="password">Répeter mot de passe:</label>
+            <br>
+            <input type="password" name="passwordVerif" id="password">
+            <br>
 
-        <?php  ?>
-        <br>
-        <input type="submit" value="S'enregistrer">
-        <br>
-        <a href="../pages/login.html" ">Se connecter</a>
-    </form>
-        <script type="text/javascript" src="../js/navbar_js.js"></script>
-        <script type="text/javascript" src="../js/footer_js.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>
+            <?php  ?>
+            <br>
+            <input type="submit" value="S'enregistrer">
+            <br>
+            <a href="../pages/login.html" ">Se connecter</a>
+        </form>
+            <script type="text/javascript" src="../js/navbar_js.js"></script>
+            <script type="text/javascript" src="../js/footer_js.js"></script>
+            <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>
 
-</body>
-    <div id="footer-container"></div>
+    </body>
+        <div id="footer-container">
+
+    </div>
 </html>
 
