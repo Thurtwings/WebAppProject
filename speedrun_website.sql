c717814296dfd272c6bc2483905637ea737cb541
Thurtwings <59878499+Thurtwings@users.noreply.github.com>
2023-04-04 09:40:44 +0200

    DONE: Updated db export

--- speedrun_website.sql
+++ speedrun_website.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : ven. 24 mars 2023 à 16:02
+-- Hôte : localhost
+-- Généré le : mar. 04 avr. 2023 à 07:38
 -- Version du serveur : 8.0.27
 -- Version de PHP : 7.4.26
 
@@ -24,18 +24,155 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `game_id` int DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `games`
+--
+
+CREATE TABLE `games` (
+  `id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rankings`
+--
+
+CREATE TABLE `rankings` (
+  `id` int NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `game_id` int DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  `time_seconds` int DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` char(60) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+  `password` char(60) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
 
+--
+-- Déchargement des données de la table `users`
+--
 
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(2, 'Thurtwings', '$2y$10$L5RlWwvcFT6aIgxXXBFbt.UBSgv/FN16yNMOpTY80DilHLH33kW1a'),
+(3, 'Steeven', '$2y$10$sPDpbfQlmBHFcigjdQCsqOM57cATm1ZBm5aY4JrB5PgLvcig84IES'),
+(4, 'Greg', '$2y$10$wJvEWXCna5WlI7ZKQ3gFiO.3AFZnzrT2T1EduviNQDfcpqD/Ad0SW'),
+(5, 'julien', '$2y$10$ni3ven7UMoitiajFVYDB5e/n2hLqI0pa66t.c9Q6EmmTPGXNYcgs.'),
+(6, 'Damien', '$2y$10$HM4w440bzUMqjZ6iPc/3aOyqF/o9nG/Ox0vW84GIVn9eIwPrNiVWS');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `videos`
+--
+
+CREATE TABLE `videos` (
+  `id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `video_url` varchar(255) NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `game_id` int DEFAULT NULL,
+  `category_id` int DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `game_id` (`game_id`);
+
+--
+-- Index pour la table `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `rankings`
+--
+ALTER TABLE `rankings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `game_id` (`game_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `game_id` (`game_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `games`
+--
+ALTER TABLE `games`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `rankings`
+--
+ALTER TABLE `rankings`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
